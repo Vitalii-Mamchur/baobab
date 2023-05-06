0010de7d2331d2b0ba816914d59221ff2ef54d94
Vitalii-Mamchur <vetalpatsi@gmail.com>
2023-05-06 22:55:24 +0300

    reset project

---
+++ js/form.js
@@ -0,0 +1,83 @@
+const TOKEN = '6208477587:AAHpTju18ANMEeQ9HRw2TKAC8CtzhrbiR-c';
+const CHAT_ID = '-1001913729529';
+const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form);
+    let formData = new FormData(form);
+
+    let message = `<b>Заявка з сайту</b>\n`;
+    message += `<b>Name: </b> ${this.name.value}\n`;
+    message += `<b>Phone: </b> ${this.phone.value}\n`;
+    this.message.value === ''
+      ? message
+      : (message += `<b>Message: </b> ${this.message.value}\n`);
+
+    if (error === 0) {
+      form.classList.add('_sending');
+
+      axios
+        .post(URI_API, {
+          chat_id: CHAT_ID,
+          text: message,
+          parse_mode: 'html',
+          disable_notification: false,
+        })
+        .then((res) => {
+          form.reset();
+          form.classList.remove('_sending');
+        })
+        .catch((err) => {
+          alert(`Похибка відправки даних форми!\n ${err.message}`);
+          form.classList.remove('_sending');
+        });
+    } else {
+      alert("Заповніть обов'язкові поля");
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+
+    for (let i = 0; i < formReq.length; i++) {
+      const input = formReq[i];
+      formRemoveError(input);
+
+      if (input.classList.contains('_phone')) {
+        if (phoneTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else {
+        if (input.value === '') {
+          formAddError(input);
+          error++;
+        }
+      }
+    }
+
+    return error;
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+  }
+
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+  }
+
+  // function for test phone
+  function phoneTest(input) {
+    return !/^((\+?3)?8)?0\d{9}$/.test(input.value);
+  }
+});
---
+++ js/script.js
@@ -0,0 +1,38 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 100;
+    let height = section.offsetHeight;
+    let id = section.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      //active navbar links
+      navLinks.forEach((link) => {
+        link.classList.remove('active');
+        link.classList &&
+          document
+            .querySelector('header nav a[href*=' + id + ']')
+            .classList.add('active');
+      });
+    }
+  });
+
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  // remove toggle icon and navbar when click navbar links (scroll)
+
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
+};
