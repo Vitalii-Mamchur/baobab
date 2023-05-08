1a60a2a6794981e8beeb34b4f9d7b564536ebafb
Vitalii-Mamchur <vetalpatsi@gmail.com>
2023-05-08 16:30:12 +0300

    corrected swiper

--- js/swiper.js
+++ js/swiper.js
@@ -2,18 +2,26 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: false,
 
-  pagination: {
-    el: '.swiper-pagination',
-    // type: 'progressbar',
-  },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   // type: 'progressbar',
+  // },
 
   autoplay: {
-    delay: 5000,
+    delay: 4000,
+    disableOnInteraction: false,
   },
+
+  speed: 800,
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
+  // effect: 'fade',
+  // fadeEffect: {
+  //   crossFade: true,
+  // },
 });
